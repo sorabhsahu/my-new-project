6a797c5428694ec6316f22b5e16bf1f7c9a58952
ssahu <sorabhkumar.sahu@merrillcorp.com>
2018-06-12 11:18:22 +0530

    GHPages lib added

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -21,7 +21,7 @@ export class AboutComponent implements OnInit {
     this._data.goal.subscribe(res=>this.goals =res);
   }
 
-  sendMeHonme(){
+  sendMeHome(){
     this.router.navigate(['']);
   }
 }
