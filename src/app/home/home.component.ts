4abe25a3a3a362ef1ec9d2b563b6cc47a122fe51
ssahu <sorabhkumar.sahu@merrillcorp.com>
2018-06-12 10:30:55 +0530

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  itemCount:number = 4;
+  btnText:string = 'Add Item';
+  goalText: string = 'my first goal';
+  goals = [];
+
+  constructor(private _data: DataService) { }
+
+  ngOnInit() {
+    this._data.goal.subscribe(res => this.goals = res);
+    this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
+  }
+
+  addItem(){
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
+  }
+
+  removeItem(i){
+    this.goals.splice(i,1);
+    this._data.changeGoal(this.goals);
+    this.itemCount = this.goals.length;
+  }
+}
